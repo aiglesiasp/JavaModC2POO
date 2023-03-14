3ba1a554df261f36c9fb79dfdc77ef08c881410c
Aitor Iglesias <iglesiaspubillaitor@gmail.com>
2023-03-14 11:10:58 +0100

    Clase Electrodomestico creada

---
+++ JavaModC2POO/src/Electrodomestico.java
@@ -0,0 +1,50 @@
+import java.awt.Color;
+
+public class Electrodomestico {
+	
+	//CONSTANTES
+	static final String colour = "blanco";
+	static final char consume = 'F';
+	static final double price = 100.0;
+	static final double weight = 5.0;
+	
+	//ATRIBUTOS
+	private double precioBase;
+	private String color;
+	private char consumoEnergetico;
+	private double peso;
+	
+	
+	//CONSTRUCTORES
+	//1-Constructor por defecto
+	public Electrodomestico() {
+		this.precioBase = price;
+		this.color = colour;
+		this.consumoEnergetico = consume;
+		this.peso = weight;
+		
+	}
+	
+	//2-Constructor con precio y peso
+	public Electrodomestico(double precio, double peso) {
+		this.precioBase = precio;
+		this.color = colour;
+		this.consumoEnergetico = consume;
+		this.peso = peso;
+		
+	}
+	
+	//3-Constructor con todos los atributos
+	public Electrodomestico(double precio, String color, char consumo, double peso) {
+		this.precioBase = precio;
+		this.color = color;
+		this.consumoEnergetico = consumo;
+		this.peso = peso;
+		
+	}
+	
+	
+	
+	
+
+}
